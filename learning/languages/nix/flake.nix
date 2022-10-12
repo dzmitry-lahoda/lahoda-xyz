6a12fb76b92d96a7fc5ca943658d06d3144fa3cd
Dzmitry Lahoda <dzmitry@lahoda.pro>
2022-10-12 10:50:44 +0100

    nix test

---
+++ learning/languages/nix/flake.nix
@@ -0,0 +1,20 @@
+{
+  description =
+    "Simple show case script showing language features and env state";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  outputs = { self, nixpkgs }: {
+    defaultPackage.x86_64-linux =
+      with import nixpkgs { system = "x86_64-linux"; };
+      stdenv.mkDerivation {
+        name = "hello";
+        src = "${self}/learning/languages/nix";
+        buildPhase = ''          
+          gcc --output hello $src/hello.c
+        '';
+        installPhase = ''
+          mkdir --parents $out/bin
+          install --target-directory $out/bin hello
+        '';
+      };
+  };
+}
