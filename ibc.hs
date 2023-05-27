093e235b0b2484ad66197d9a9ad7d945e26faacb
dzmitry-lahoda <dzmitry@lahoda.pro>
2023-05-27 10:03:20 +0100

    packet

--- ibc.hs
+++ ibc.hs
@@ -3,19 +3,56 @@
 
 data ConsensusState = ConsensusState { height:: Integer } deriving (Show)
 
-data Connection = Connection { id :: String } deriving (Show)
+data Connection = Connection { id :: String } deriving (Show, Eq)
 
 data Port = Port Integer
+
+validate :: ConsensusState -> Bool
+validate (ConsensusState height) = height == 42
 
 -- authenticateCapability
 -- hash
 
 -- Identifier, get, set, delete, getCurrentHeight, 
 
-data ChannelState = ChannelState String
-data ChannelOrder = Ordered | Unordered | OrderedAllowTimeout
+data ChannelOrder = 
+    Ordered 
+    -- An unordered channel is a channel where packets can be delivered in any order, which may differ from the order in which they were sent.
+    | Unordered 
+    | OrderedAllowTimeout
 data Identifier = Identifier String
-data ChannelEnd = ChannelEnd ChannelState Identifier Identifier [Identifier] String
+
+data ChannelState = 
+  -- state has just started the opening handshake.
+  Init 
+  -- state has acknowledged the handshake step on the counterparty chain
+  | TryOpen 
+  -- state has completed the handshake and is ready to send and receive packets
+  | Open 
+  -- state has been closed and can no longer be used to send or receive packets
+  | Closed
+
+data ChannelEnd = ChannelEnd 
+  { 
+    -- the current state of the channel end
+    state:: ChannelState, 
+    ordering:: ChannelOrder,
+    --  identifies the port on the counterparty chain which owns the other end of the channel
+    counterPartyPortIdentifier :: Identifier,  
+    -- identifies the channel end on the counterparty chain
+    counterpartyChannelIdentifier :: Identifier,
+    -- stores the list of connection identifiers, in order, along which packets sent on this channel will travel
+    connectionHops :: [Identifier],
+    -- an opaque channel version, which is agreed upon during the handshake. This can determine module-level configuration such as which packet encoding is used for the channel. This version is not used by the core IBC protocol
+    version :: String,
+    -- tracks the sequence number for the next packet to be sent
+    nextSequenceSend :: Int,
+    -- tracks the sequence number for the next packet to be received
+    nextSequenceRecv :: Int,
+    -- tracks the sequence number for the next packet to be acknowledged
+    nextSequenceAck :: Int
+    }
+
 
 main = do
   putStrLn "Hello world from a distributable Haskell script!"
