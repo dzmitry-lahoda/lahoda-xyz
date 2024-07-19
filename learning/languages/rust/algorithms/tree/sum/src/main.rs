1db5de678d134046582d6635e8ea01409e582bb2
dzmitry-lahoda <dzmitry@lahoda.pro>
2024-07-19 23:06:13 +0100

    abstraced tree

--- learning/languages/rust/algorithms/tree/sum/src/main.rs
+++ learning/languages/rust/algorithms/tree/sum/src/main.rs
@@ -1,51 +1,72 @@
-struct AvlNode {
-    left: Option<Box<AvlNode>>,
-    right: Option<Box<AvlNode>>,
-    key: i32,
-    value: i32,
-    height: i32,
+enum Node<T> {
+    Leaf(T),
+    Left(T, Box<Node<T>>),
+    Right(T, Box<Node<T>>),
+    Both(T, Box<Node<T>>, Box<Node<T>>),
 }
 
-impl AvlNode {
-    fn new_leaf(key: i32, value: i32) -> AvlNode {
-        AvlNode {
-            left: None,
-            right: None,
-            value,
-            key,
-            height: 0,
-        }
-    }
+struct LeveledPayload<P, L> {
+    payload: P,
+    level: L,
+}
 
-    fn new_root(key: i32, value: i32, left: AvlNode, right: AvlNode) -> AvlNode {
-        AvlNode {
-            left: Some(Box::new(left)),
-            right: Some(Box::new(right)),
-            value,
-            key,
-            height: 0,
-        }
-    }
+struct AvlNode<K, M> {
+    left: Option<Box<AvlNode<K, M>>>,
+    right: Option<Box<AvlNode<K, M>>>,
+    key: K,
+    meta: M,
+}
 
-    /// has search property
-    fn is_search(&self) -> bool{
+impl<K, M> AvlNode<K, M> {
+    // fn new_leaf(key: K, payload: i32) -> Self {
+    //     Self {
+    //         left: None,
+    //         right: None,
+    //         key,
+    //     }
+    // }
+
+    // fn new_root(key: K, value: i32, left: Self, right: Self) -> Self {
+    //     Self {
+    //         left: Some(Box::new(left)),
+    //         right: Some(Box::new(right)),
+    //         payload: value,
+    //         key,
+    //         level: 0,
+    //     }
+    // }
+
+    fn height(&self) -> i32 {
         match (&self.left, &self.right) {
-            (None, None) => true,
-            (None, Some(right)) => right.is_search() && self.key < right.key,
-            (Some(left), None) => left.is_search() && self.key > left.key,
-            (Some(left), Some(right)) => left.is_search() && right.is_search() && self.key > left.key && self.key < right.key,
+            (Some(left), Some(right)) => 1 + std::cmp::max(left.height(), right.height()),
+            (Some(left), None) => 1 + left.height(),
+            (None, Some(right)) => 1 + right.height(),
+            (None, None) => 0,
         }
-        // let left = self.left.map(|x| x.is_search()).unwrap_or(true);
-        // let right = self.right.map(|x| x.is_search()).unwrap_or(true);
     }
 
-    fn is_balances(&self, disbalance: u8) -> bool {
+    fn is_balanced(&self, disbalance: u8) -> bool {
         todo!()
     }
 }
 
-struct Tree {
-    root: AvlNode,
+impl<K: core::cmp::PartialOrd, M> AvlNode<K, M> {
+    /// has search property
+    fn is_search(&self) -> bool {
+        match (&self.left, &self.right) {
+            (None, Some(right)) => right.is_search() && self.key < right.key,
+            (Some(left), None) => left.is_search() && self.key > left.key,
+            (Some(left), Some(right)) => {
+                left.is_search() && right.is_search() && self.key > left.key && self.key < right.key
+            }
+            (None, None) => true,
+        }
+    }
+}
+
+struct Tree<K, M> {
+    root: AvlNode<K, M>,
+    _marker: std::marker::PhantomData<(K, M)>,
 }
 
 fn main() {
@@ -54,19 +75,17 @@ fn main() {
             left: None,
             right: None,
             key: 1,
-            value: 13,
-            height: 1,    
+            meta : (),
         })),
         right: Some(Box::new(AvlNode {
             left: None,
             right: None,
             key: 17,
-            value: 42,
-            height: 1,    
+            meta : (),
         })),
-        key: 10,
-        value: 2,
-        height: 0,
+        key: 10i32,
+        meta : (),
     };
     assert!(tree.is_search());
+    assert_eq!(tree.height(), 1);
 }
