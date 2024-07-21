7638eca3c614bba88542ea1489509516f5354c26
dzmitry-lahoda <dzmitry@lahoda.pro>
2024-07-21 12:51:54 +0100

    makeing tree typed

--- learning/languages/rust/algorithms/tree/sum/src/main.rs
+++ learning/languages/rust/algorithms/tree/sum/src/main.rs
@@ -5,19 +5,25 @@ enum Node<T> {
     Both(T, Box<Node<T>>, Box<Node<T>>),
 }
 
-struct LeveledPayload<P, L> {
-    payload: P,
+struct LeveledPayloadAugmentation<L> {
     level: L,
 }
 
-struct AvlNode<K, M> {
-    left: Option<Box<AvlNode<K, M>>>,
-    right: Option<Box<AvlNode<K, M>>>,
+struct AvlNode<K, P, A> {
+    children: [Option<Box<AvlNode<K, P,  A>>>; 2],
     key: K,
-    meta: M,
+    augmentation: A,
+    payload: P,
 }
 
-impl<K, M> AvlNode<K, M> {
+trait TreeConfig {
+    type Tree;
+    type Key;
+    type Payload;
+    type Augmentation;
+}
+
+impl<K, P, A,> AvlNode<K, P, A> {
     // fn new_leaf(key: K, payload: i32) -> Self {
     //     Self {
     //         left: None,
@@ -37,23 +43,57 @@ impl<K, M> AvlNode<K, M> {
     // }
 
     fn height(&self) -> i32 {
-        match (&self.left, &self.right) {
-            (Some(left), Some(right)) => 1 + std::cmp::max(left.height(), right.height()),
-            (Some(left), None) => 1 + left.height(),
-            (None, Some(right)) => 1 + right.height(),
-            (None, None) => 0,
-        }
+        let left = self.children[0].as_ref().map(|x| x.height()).unwrap_or(-1);
+        let right = self.children[1].as_ref().map(|x| x.height()).unwrap_or(-1);
+        1 + std::cmp::max(left, right)
     }
 
     fn is_balanced(&self, disbalance: u8) -> bool {
         todo!()
     }
+
+    // fn insert(&mut self, key: K, payload: P) -> Option<P> {
+    //     // insert into binary tree
+    //     match self.key.cmp(&key) {
+    //         std::cmp::Ordering::Less => {
+    //             if let Some(right) = &mut self.children[1] {
+    //                 right.insert(key, payload)
+    //             } else {
+    //                 self.children[1] = Some(Box::new(AvlNode {
+    //                     children: [None, None],
+    //                     key,
+    //                     augmentation: (),
+    //                     payload,
+    //                 }));
+    //                 None
+    //             }
+    //         }
+    //         std::cmp::Ordering::Greater => {
+    //             if let Some(left) = &mut self.children[0] {
+    //                 left.insert(key, payload)
+    //             } else {
+    //                 self.children[0] = Some(Box::new(AvlNode {
+    //                     children: [None, None],
+    //                     key,
+    //                     augmentation: (),
+    //                     payload,
+    //                 }));
+    //                 None
+    //             }
+    //         }
+    //         std::cmp::Ordering::Equal => {
+    //             let old_payload = std::mem::replace(&mut self.payload, payload);
+    //             Some(old_payload)
+    //         }
+    //     }
+        // None
+    }
 }
 
-impl<K: core::cmp::PartialOrd, M> AvlNode<K, M> {
+impl<K: core::cmp::PartialOrd, P, A> AvlNode<K, P, A> {
     /// has search property
     fn is_search(&self) -> bool {
-        match (&self.left, &self.right) {
+        match (&self.children[0], &self.children[1]) {
             (None, Some(right)) => right.is_search() && self.key < right.key,
             (Some(left), None) => left.is_search() && self.key > left.key,
             (Some(left), Some(right)) => {
@@ -64,27 +104,30 @@ impl<K: core::cmp::PartialOrd, M> AvlNode<K, M> {
     }
 }
 
-struct Tree<K, M> {
-    root: AvlNode<K, M>,
-    _marker: std::marker::PhantomData<(K, M)>,
+struct Tree<K, P, A> {
+    root: AvlNode<K,P, A>,
+    _marker: std::marker::PhantomData<(K, P, A)>,
 }
 
 fn main() {
-    let tree = AvlNode {
-        left: Some(Box::new(AvlNode {
-            left: None,
-            right: None,
-            key: 1,
-            meta : (),
-        })),
-        right: Some(Box::new(AvlNode {
-            left: None,
-            right: None,
-            key: 17,
-            meta : (),
-        })),
+    let tree = AvlNode::<_, _, ()> {
+        children: [
+            Some(Box::new(AvlNode {
+                children: [None, None],
+                key: 1,
+                augmentation: (),
+                payload: (),
+            })),
+            Some(Box::new(AvlNode {
+                children: [None, None],
+                key: 17,
+                augmentation: (),
+                payload: (),
+            })),
+        ],
         key: 10i32,
-        meta : (),
+        augmentation: (),
+        payload: (),
     };
     assert!(tree.is_search());
     assert_eq!(tree.height(), 1);
