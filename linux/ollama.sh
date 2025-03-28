8d5ebb615c507660265575192bc0807d931d8ef7
dzmitry-lahoda <dzmitry@lahoda.pro>
2025-03-28 11:44:32 +0000

    lets go

--- linux/ollama.sh
+++ linux/ollama.sh
@@ -2,7 +2,7 @@ curl -fsSL https://ollama.com/install.sh | sh
 ollama pull qwen2.5-coder:1.5b-base
 ollama pull nomic-embed-text:latest
 ollama pull llama3.1:8b
-
+ollama pull gemma3:4b
 
 
 # generate me echo 42
