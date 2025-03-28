1914391f637ec7376ea9f5bf20652a1ed16e4d51
dzmitry-lahoda <dzmitry@lahoda.pro>
2025-03-28 12:14:30 +0000

    gpu and display info

---
+++ nix/gpu-info.sh
@@ -0,0 +1,19 @@
+if command -v glxinfo > /dev/null; then
+  glxinfo
+fi
+
+if command -v lspci > /dev/null; then
+  lspci | grep VGA
+fi
+
+if command -v lshw > /dev/null; then
+  lshw -class display
+fi
+
+if command -v xrandr > /dev/null; then
+  xrandr --listmonitors
+fi
+
+if command -v nvidia-smi > /dev/null; then
+  nvidia-smi
+fi
