9edaf9cfeac6204f555fbb77be61d684efca8161
dzmitry-lahoda <dzmitry@lahoda.pro>
2025-03-31 18:28:30 +0100

    better autocomplete

--- linux/ollama.sh
+++ linux/ollama.sh
@@ -1,11 +1,10 @@
 curl -fsSL https://ollama.com/install.sh | sh
-ollama pull qwen2.5-coder:1.5b-base
 ollama pull nomic-embed-text:latest
 ollama pull llama3.1:8b
 ollama pull gemma3:4b
 ollama pull gemma3:12b
 
-
-
-# generate me echo 42
-echo  
+# autocomplete, tried `qwen2.5-coder:1.5b-base` - it is more stupid of ms gh copilot
+ollama pull qwen2.5-coder:3b
+ollama pull qwen2.5-coder:7b
+ollama pull qwen2.5-coder:14b
